8526c79a368c90b5611187d1e11470f5009b032d
Kintaro-Oe <marcusclairmont@gmail.com>
2019-04-06 11:49:50 +0100

    Encapsulate the calculator file into classes based on behaviour

--- week-1/oop_1/lib/calculator.rb
+++ week-1/oop_1/lib/calculator.rb
@@ -1,11 +1,16 @@
-def add(number_1, number_2)
-  number_1 + number_2
+# Understands how to perform operations on two numbers. Addition and subtraction.
+class Calculator
+  def add(number_1, number_2)
+    number_1 + number_2
+  end
+  def subtract(number_1, number_2)
+    number_1 - number_2
+  end
 end
 
-def subtract(number_1, number_2)
-  number_1 - number_2
-end
-
-def print_answer(answer)
-  "The Answer is: #{ answer }"
+# Understands how to return the argument as a pretty string
+class Printer
+  def print_answer(answer)
+    "The Answer is: #{ answer }"
+  end
 end
